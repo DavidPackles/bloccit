74a000828d57f6c4855fb6e208fd90057f7a12ac
David Packles <DavidPackles@gmail.com>
2014-10-02 22:09:29 -0400

    added user associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,31 @@
      body: Faker::Lorem.paragraph
    )
  end
- 
+
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+  puts "#{user.name} saved!"
+end
+
+users = User.all
+
+
+# Why doesn't this work?
+User.first.update_attributes!(
+  email: 'davidpackles@gmail.com',
+  password: 'fortnight',
+)
+
+
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "You can sign in by using #{User.first.email}"
+
