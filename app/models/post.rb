81a5a8975940ebea88d65c20903a0f293f38f8fb
David Packles <DavidPackles@gmail.com>
2014-10-11 09:34:36 -0400

    Refactored redcarpet to live in the model

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,18 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  def markdown_title
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render self.title).html_safe
+  end
+
+  def markdown_body
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render self.body).html_safe
+  end
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,18 @@
 class Topic < ActiveRecord::Base
   has_many :posts
+
+  def markdown_title
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render self.title).html_safe
+  end
+
+  def markdown_description
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render self.description).html_safe
+  end
+
 end
