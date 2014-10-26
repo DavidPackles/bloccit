0d55a0e2753a7f594b3ebe70238b7714ec8fc950
David Packles <DavidPackles@gmail.com>
2014-10-26 13:22:34 -0400

    Added rspec testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,13 +11,10 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   mount_uploader :image, ImageUploader
-
-  after_create :create_vote
-
 
   def markdown_title
     render_markdown(self.title)
@@ -46,10 +43,8 @@ class Post < ActiveRecord::Base
     update_attribute(:rank, new_rank)
   end
 
-  private
-
   def create_vote
-    user.votes.create( value:1, post: self)
+    user.votes.create( value: 1, post: self)
   end
 
   protected
