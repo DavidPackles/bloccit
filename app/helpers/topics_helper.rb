2f6a769f99665a202567518e04c8e075227469b5
David Packles <DavidPackles@gmail.com>
2014-10-30 21:12:19 -0400

    Refactored voting arrows in application helper

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,11 @@
 module TopicsHelper
+
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+
+  def up_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
+
 end
