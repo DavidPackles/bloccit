8f60f8dccb16105d163f7fa4e2924e3399dd0281
David Packles <DavidPackles@gmail.com>
2014-11-03 23:24:08 -0500

    added factory girl and user testing

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
  describe Vote do
-  include TestFactories
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
 
         expect(Vote.new(value:2).valid?).to eq(false)
         expect(Vote.new(value:1).valid?).to eq(true)
-        expect(Vote.new(value:-1).valid?).to eq (true)          
-      
+        expect(Vote.new(value:-1).valid?).to eq (true)
+
       end
     end
   end
 
   describe 'after_save' do
     it "calls 'Post#update_rank' after save" do
-      post = associated_post
+      post = create(:post)
       vote = Vote.new(value: 1, post: post)
       expect(post).to receive(:update_rank)
       vote.save
